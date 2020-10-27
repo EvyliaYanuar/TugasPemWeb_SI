0b8794ad04662265d22f2d4fce6770231324548a
Evylia Yanuar <61932543+EvyliaYanuar@users.noreply.github.com>
2020-10-27 07:29:28 +0700

    Add files via upload

---
+++ Form_Evylia Yanuar Laily_0110119043/daftar.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+echo "<center><h2>TERIMAKASIH SUDAH MENDAFTAR</h2></center>";
+echo "<center>Data Anda Sudah Disimpan</center>";
+echo "<center>Tunggu Info Selanjutnya Dari Kami</center";
+
+?>
